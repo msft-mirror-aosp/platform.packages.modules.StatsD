f130493480d289a8d2a6606b670d5832de4e811c
tsaichristine <tsaichristine@google.com>
2023-02-13 17:53:58 -0800

    Add global condition timing to DurationMetricProducer
    Test: atest statsd_test
    Bug: 268531567
    Change-Id: I61f4c104dd53a97f9c4eeb92660d140ba6c01a5a

--- statsd/src/metrics/duration_helper/MaxDurationTracker.cpp
+++ statsd/src/metrics/duration_helper/MaxDurationTracker.cpp
@@ -166,6 +166,7 @@ void MaxDurationTracker::noteStopAll(const int64_t eventTime) {
 
 bool MaxDurationTracker::flushCurrentBucket(
         const int64_t& eventTimeNs, const optional<UploadThreshold>& uploadThreshold,
+        const int64_t globalConditionTrueNs,
         std::unordered_map<MetricDimensionKey, std::vector<DurationBucket>>* output) {
     VLOG("MaxDurationTracker flushing.....");
 
@@ -200,6 +201,7 @@ bool MaxDurationTracker::flushCurrentBucket(
         info.mBucketStartNs = mCurrentBucketStartTimeNs;
         info.mBucketEndNs = currentBucketEndTimeNs;
         info.mDuration = mDuration;
+        info.mConditionTrueNs = globalConditionTrueNs;
         (*output)[mEventKey].push_back(info);
         VLOG("  final duration for last bucket: %lld", (long long)mDuration);
     } else {
@@ -226,7 +228,7 @@ bool MaxDurationTracker::flushIfNeeded(
     if (eventTimeNs < getCurrentBucketEndTimeNs()) {
         return false;
     }
-    return flushCurrentBucket(eventTimeNs, uploadThreshold, output);
+    return flushCurrentBucket(eventTimeNs, uploadThreshold, /*globalConditionTrueNs*/ 0, output);
 }
 
 void MaxDurationTracker::onSlicedConditionMayChange(bool overallCondition,
