980429272dbbd472cddad0d6e6677db0765ecbea
Raymond Hernandez <rayhdez@google.com>
2023-05-04 11:21:39 -0700

    Cleanup Limit Pull Flag
    Bug: 280835984
    Test: atest statsd_test
    Change-Id: Ib1fab79d2b97f7165a679e3b54b33e344ddad0a0
    Merged-In: Ib1fab79d2b97f7165a679e3b54b33e344ddad0a0

--- statsd/src/main.cpp
+++ statsd/src/main.cpp
@@ -76,7 +76,7 @@ int main(int /*argc*/, char** /*argv*/) {
             std::make_shared<LogEventQueue>(4000 /*buffer limit. Buffer is NOT pre-allocated*/);
 
     // Initialize boot flags
-    FlagProvider::getInstance().initBootFlags({LIMIT_PULL_FLAG});
+    FlagProvider::getInstance().initBootFlags({});
 
     sp<UidMap> uidMap = UidMap::getInstance();
 
