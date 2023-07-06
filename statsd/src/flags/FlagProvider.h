078ca0d4737f2f96c662766df943c872acf2afa5
Raymond Hernandez <rayhdez@google.com>
2023-07-06 15:10:08 -0700

    Increase event queue size to 50k
    Increase event queue size to 50k. Flag guarded.
    
    Bug: 290236224
    Test: atest statsd_test
    Test: m -j
    Ignore-AOSP-First: udc-mainline-prod feature
    Change-Id: I5da8b443d00a0b31703b3ee1adf005b4e161fc06

--- statsd/src/flags/FlagProvider.h
+++ statsd/src/flags/FlagProvider.h
@@ -39,6 +39,7 @@ const std::string STATSD_NATIVE_BOOT_NAMESPACE = "statsd_native_boot";
 
 const std::string OPTIMIZATION_SOCKET_PARSING_FLAG = "optimization_socket_parsing";
 const std::string STATSD_INIT_COMPLETED_NO_DELAY_FLAG = "statsd_init_completed_no_delay";
+const std::string INCREASE_EVENT_QUEUE_50000_FLAG = "increase_event_queue_50000";
 
 const std::string FLAG_TRUE = "true";
 const std::string FLAG_FALSE = "false";
