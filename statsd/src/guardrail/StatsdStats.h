71fffbb7c332ce1449bc3d4858fc83cc315415f8
Raymond Hernandez <rayhdez@google.com>
2023-07-24 12:48:02 -0700

    Add DataCorruptReason to ConfigMetricsReport
    Test: atest statsd_test
    Bug: 292568772
    Change-Id: I2603227f98cd1395221739c1f1baf6d9da6d1d93
    Merged-In: I2603227f98cd1395221739c1f1baf6d9da6d1d93

--- statsd/src/guardrail/StatsdStats.h
+++ statsd/src/guardrail/StatsdStats.h
@@ -532,6 +532,16 @@ public:
      */
     static std::pair<size_t, size_t> getAtomDimensionKeySizeLimits(const int atomId = -1);
 
+    /**
+     * Returns true if there is recorded event queue overflow
+     */
+    bool hasEventQueueOverflow() const;
+
+    /**
+     * Returns true if there is recorded socket loss
+     */
+    bool hasSocketLoss() const;
+
     typedef struct PullTimeoutMetadata {
         int64_t pullTimeoutUptimeMillis;
         int64_t pullTimeoutElapsedMillis;
